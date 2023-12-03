fa6ab45a7733e6127a2da931ee4e34d5b072d319
Roy Wallace <waldo000000@gmail.com>
2023-12-03 15:50:54 +1000

    Day 1; part 1

--- AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
@@ -1,5 +1,19 @@
-﻿namespace AdventOfCode;
+﻿namespace AdventOfCode.Day1;
 
 public static class Day1Puzzle
 {
+    public static int GetSumOfCalibrationValues(string[] calibrationValues)
+    {
+        return calibrationValues
+            .Select(GetCalibrationValue)
+            .ToArray()
+            .Sum();
+    }
+
+    private static int GetCalibrationValue(string calibrationValue)
+    {
+        var firstDigit = calibrationValue.First(char.IsDigit);
+        var lastDigit = calibrationValue.Last(char.IsDigit);
+        return int.Parse($"{firstDigit}{lastDigit}");
+    }
 }
--- AdventOfCode/AdventOfCodeTests/Day1/Day1Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day1/Day1Tests.cs
@@ -1,12 +1,30 @@
+using AdventOfCode;
+using AdventOfCode.Day1;
 using NUnit.Framework;
+using FluentAssertions;
 
 namespace AdventOfCodeTests.Day1;
 
 public class Day1Tests
 {
     [Test]
-    public void Test1()
+    public void GetSumOfCalibrationValue_SampleData_ReturnsExpectedResult()
     {
-        Assert.Pass();
+        var frequencyChanges = ReadCalibrationDocument("Day1\\Part1.sample.txt");
+        var sumOfCalibrationValues = Day1Puzzle.GetSumOfCalibrationValues(frequencyChanges);
+        sumOfCalibrationValues.Should().Be(142);
+    }
+    
+    [Test]
+    public void GetSumOfCalibrationValue_RealData_ReturnsExpectedResult()
+    {
+        var frequencyChanges = ReadCalibrationDocument("Day1\\Part1.real.txt");
+        var sumOfCalibrationValues = Day1Puzzle.GetSumOfCalibrationValues(frequencyChanges);
+        sumOfCalibrationValues.Should().Be(53080);
+    }
+
+    private string[] ReadCalibrationDocument(string inputFilePath)
+    {
+        return File.ReadLines(inputFilePath).ToArray();
     }
 }
